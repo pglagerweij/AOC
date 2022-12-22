4abe13d79d10b0d3177bfaf4c8c2476614f166fb
Pascal Lagerweij <pascallagerweij@gmail.com>
2022-12-22 07:41:06 +0100

    start of day 22

---
+++ 2022/22/01/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var inputFile string = "trial.txt"
+
+// var inputFile string = "input.txt"
+
+var directions map[int]string = map[int]string{
+	0: "Right",
+	1: "Down",
+	2: "Left",
+	3: "Up",
+}
+
+func main() {
+	input, _ := os.ReadFile(inputFile)
+	total := strings.Split(string(input), "\n\n")
+	drawing := total[0]
+	instructions := total[1]
+	currentPosition := 0
+	// Construct Map element
+	for _, row := range strings.Split(drawing, "\n") {
+		fmt.Printf("%v\n", row)
+	}
+
+	// Loop over instructions
+	for _, s := range strings.Split(strings.ReplaceAll(strings.ReplaceAll(instructions, "R", ";R;"), "L", ";L;"), ";") {
+		if s == "L" {
+			currentPosition = (currentPosition - 1) % 4
+			continue
+		} else if s == "R" {
+			currentPosition = (currentPosition + 1) % 4
+			continue
+		}
+
+		sint, _ := strconv.Atoi(s)
+		fmt.Printf("The value is %v\n", sint)
+
+	}
+
+}
